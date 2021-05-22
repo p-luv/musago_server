06c3ceb7852d2ad0087941d5207aae9989f10a67
omtye <developerjung94@naver.com>
2021-05-23 05:00:14 +0900

    set aws-ec2 url

--- myapi/settings.py
+++ myapi/settings.py
@@ -44,10 +44,10 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
 
     # rest_auth
-    'rest_auth',
+    #'rest_auth',
     'allauth',
     'allauth.account',
-    'rest_auth.registration',
+   # 'rest_auth.registration',
 
 ]
 
